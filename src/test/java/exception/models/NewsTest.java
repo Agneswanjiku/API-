7dc158274bf24836486654fe70e2db7c3e7333c0
edithntinyari <emothegi@gmail.com>
2020-07-23 19:55:55 +0300

    added test

---
+++ src/test/java/exception/models/NewsTest.java
@@ -0,0 +1,31 @@
+package exception.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class NewsTest {
+
+    @Test
+    public void setId() throws Exception{
+        News testNews = setUpNews();
+        testNews.setId(3);
+        assertNotEquals(2,testNews.getId());
+    }
+
+
+
+    @Test
+    public void News_instantiatesWithCorrectValues() throws Exception{
+        News testnews = setUpNews();
+        assertEquals("clientNews", testnews.getClientNews());
+        assertEquals("collageNews",testnews.getCollegeNews());
+
+    }
+    public News setUpNews(){
+        return new News("clientNews", "collageNews");
+    }
+
+
+}
